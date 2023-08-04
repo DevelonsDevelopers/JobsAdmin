3bb72d4f8fd5ca2b8737a09e74c33403e51f0b35
AhmadHassan <AhmadHassan25202@gmail.com>
2023-08-04 10:22:09 -0700

    2

--- src/dashboard/Portal/Sidebar.jsx
+++ src/dashboard/Portal/Sidebar.jsx
@@ -1,11 +1,13 @@
-import { forwardRef } from "react";
-import { HiOutlineHome, HiOutlineUser, HiOutlineCblueitCard } from "react-icons/hi";
+import { forwardRef, useState } from "react";
+import { HiOutlineHome, HiOutlineUser, HiOutlineCblueitCard, HiOutlineUsers } from "react-icons/hi";
 
-import { RiPagesLine } from 'react-icons/ri'
-import { FaPager, FaTable, FaUser,FaUserClock } from 'react-icons/fa'
+import { RiEarthFill, RiEarthLine, RiHomeOfficeLine, RiPagesLine, RiUserSearchLine } from 'react-icons/ri'
+import { FaBoxes, FaPager, FaRegListAlt, FaTable, FaUser,FaUserCheck,FaUserClock } from 'react-icons/fa'
+import { BiCategoryAlt,BiUserCheck } from 'react-icons/bi'
+// import {FiUserCheck} from 'react-icons/fi'
 
-import { AiOutlineUserSwitch ,AiOutlineCloseCircle } from 'react-icons/ai'
-import { PiUsersThreeBold } from 'react-icons/pi'
+import { AiOutlineUserSwitch ,AiOutlineCloseCircle, AiOutlineTag, AiOutlineTransaction, AiOutlineOrderedList, AiOutlineNotification } from 'react-icons/ai'
+import { PiBriefcaseLight, PiBuildingsLight, PiUsersThreeBold } from 'react-icons/pi'
 import { Button } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 
@@ -13,19 +15,20 @@ import { useNavigate } from "react-router-dom";
 
  const Sidebar = forwardRef(({ showNav }, ref) => {
   const router = useNavigate();
+  const [active , isActive] = useState("false")
 
   return (
-    <div ref={ref} className="mt-[-4rem] fixed w-56 h-full bg-white shadow-sm max-md:w-[35%] overflow-auto ">
+    <div ref={ref} className="mt-[-4rem] fixed w-56 h-full bg-white shadow-sm max-md:w-[35%] overflow-auto no-scrollbar">
     
 
       <div className="flex flex-col mt-[2rem] ">
         <div onClick={() => router('/')}>
           <div
-            className={`pl-7 py-3 mx-5 rounded text-center cursor-pointer mb-3 flex items-center transition-colors 
+            className={`pl-7 py-3 mx-5 rounded text-center cursor-pointer mb-1 flex items-center transition-colors 
             ${
               router.pathname === "/"
                 ? "bg-blue-100 text-blue-500"
-                : "text-gray-400 hover:bg-blue-100 hover:text-blue-500"
+                : "text-black font-[600]  hover:text-white  hover:bg-gradient-to-r from-sky-600 to-cyan-400"
             }
             `}
           >
@@ -41,16 +44,14 @@ import { useNavigate } from "react-router-dom";
       <div className="flex flex-col ">
         <div onClick={() => router('/categories')}>
           <div
-            className={`pl-7 py-3 mx-5 rounded text-center cursor-pointer mb-3 flex items-center transition-colors 
-            ${
-              router.pathname === "/categories"
-                ? "bg-blue-100 text-blue-500"
-                : "text-gray-400 hover:bg-blue-100 hover:text-blue-500"
-            }
+          
+            // className={`px-2 py-2.5 hover:bg-cprimary-300 hover:text-csecond-100 rounded-md transition  `}
+            className={`pl-7 py-3 mx-5 rounded text-center cursor-pointer mb-1 flex items-center transition-colors 
+            ${isActive && router === "/categories" ? "bg-blue-100 text-blue-500": "text-black font-[600] hover:text-white  hover:bg-gradient-to-r from-sky-600 to-cyan-400 hover:bg-blue-100 hover:text-blue-500"}
             `}
           >
             <div className="mr-2">
-              <HiOutlineHome className="h-5 w-5" />
+              <BiCategoryAlt className="h-5 w-5" />
             </div>
             <div>
               <p>Categories</p>
@@ -61,16 +62,16 @@ import { useNavigate } from "react-router-dom";
       <div className="flex flex-col ">
         <div onClick={() => router('/countries')}>
           <div
-            className={`pl-7 py-3 mx-5 rounded text-center cursor-pointer mb-3 flex items-center transition-colors 
+            className={`pl-7 py-3 mx-5 rounded text-center cursor-pointer mb-1 flex items-center transition-colors 
             ${
               router.pathname === "/countries"
                 ? "bg-blue-100 text-blue-500"
-                : "text-gray-400 hover:bg-blue-100 hover:text-blue-500"
+                : "text-black font-[600] hover:text-white  hover:bg-gradient-to-r from-sky-600 to-cyan-400 hover:bg-blue-100 hover:text-blue-500"
             }
             `}
           >
             <div className="mr-2">
-              <HiOutlineHome className="h-5 w-5" />
+            <RiEarthLine className="h-5 w-5" />
             </div>
             <div>
               <p>Countries</p>
@@ -81,16 +82,16 @@ import { useNavigate } from "react-router-dom";
       <div className="flex flex-col ">
         <div onClick={() => router('/cities')}>
           <div
-            className={`pl-7 py-3 mx-5 rounded text-center cursor-pointer mb-3 flex items-center transition-colors 
+            className={`pl-7 py-3 mx-5 rounded text-center cursor-pointer mb-1 flex items-center transition-colors 
             ${
               router.pathname === "/cities"
                 ? "bg-blue-100 text-blue-500"
-                : "text-gray-400 hover:bg-blue-100 hover:text-blue-500"
+                : "text-black font-[600] hover:text-white  hover:bg-gradient-to-r from-sky-600 to-cyan-400 hover:bg-blue-100 hover:text-blue-500"
             }
             `}
           >
             <div className="mr-2">
-              <HiOutlineHome className="h-5 w-5" />
+              <PiBuildingsLight className="h-5 w-5" />
             </div>
             <div>
               <p>Cities</p>
@@ -101,16 +102,16 @@ import { useNavigate } from "react-router-dom";
       <div className="flex flex-col ">
         <div onClick={() => router('/jobs')}>
           <div
-            className={`pl-7 py-3 mx-5 rounded text-center cursor-pointer mb-3 flex items-center transition-colors 
+            className={`pl-7 py-3 mx-5 rounded text-center cursor-pointer mb-1 flex items-center transition-colors 
             ${
               router.pathname === "/jobs"
                 ? "bg-blue-100 text-blue-500"
-                : "text-gray-400 hover:bg-blue-100 hover:text-blue-500"
+                : "text-black font-[600] hover:text-white  hover:bg-gradient-to-r from-sky-600 to-cyan-400 hover:bg-blue-100 hover:text-blue-500"
             }
             `}
           >
             <div className="mr-2">
-              <HiOutlineHome className="h-5 w-5" />
+              <PiBriefcaseLight className="h-5 w-5" />
             </div>
             <div>
               <p>Jobs</p>
@@ -121,16 +122,16 @@ import { useNavigate } from "react-router-dom";
       <div className="flex flex-col ">
         <div onClick={() => router('/companies')}>
           <div
-            className={`pl-7 py-3 mx-5 rounded text-center cursor-pointer mb-3 flex items-center transition-colors 
+            className={`pl-7 py-3 mx-5 rounded text-center cursor-pointer mb-1 flex items-center transition-colors 
             ${
               router.pathname === "/companies"
                 ? "bg-blue-100 text-blue-500"
-                : "text-gray-400 hover:bg-blue-100 hover:text-blue-500"
+                : "text-black font-[600] hover:text-white  hover:bg-gradient-to-r from-sky-600 to-cyan-400 hover:bg-blue-100 hover:text-blue-500"
             }
             `}
           >
             <div className="mr-2">
-              <HiOutlineHome className="h-5 w-5" />
+              <RiHomeOfficeLine className="h-5 w-5" />
             </div>
             <div>
               <p>Companies</p>
@@ -141,16 +142,16 @@ import { useNavigate } from "react-router-dom";
       <div className="flex flex-col ">
         <div onClick={() => router('/tags')}>
           <div
-            className={`pl-7 py-3 mx-5 rounded text-center cursor-pointer mb-3 flex items-center transition-colors 
+            className={`pl-7 py-3 mx-5 rounded text-center cursor-pointer mb-1 flex items-center transition-colors 
             ${
               router.pathname === "/tags"
                 ? "bg-blue-100 text-blue-500"
-                : "text-gray-400 hover:bg-blue-100 hover:text-blue-500"
+                : "text-black font-[600] hover:text-white  hover:bg-gradient-to-r from-sky-600 to-cyan-400 hover:bg-blue-100 hover:text-blue-500"
             }
             `}
           >
             <div className="mr-2">
-              <HiOutlineHome className="h-5 w-5" />
+              <AiOutlineTag className="h-5 w-5" />
             </div>
             <div>
               <p>Tags</p>
@@ -161,16 +162,16 @@ import { useNavigate } from "react-router-dom";
       <div className="flex flex-col ">
         <div onClick={() => router('/appliedusers')}>
           <div
-            className={`pl-7 py-3 mx-5 rounded text-center cursor-pointer mb-3 flex items-center transition-colors 
+            className={`pl-7 py-3 mx-5 rounded text-center cursor-pointer mb-1 flex items-center transition-colors 
             ${
               router.pathname === "/appliedusers"
                 ? "bg-blue-100 text-blue-500"
-                : "text-gray-400 hover:bg-blue-100 hover:text-blue-500"
+                : "text-black font-[600] hover:text-white  hover:bg-gradient-to-r from-sky-600 to-cyan-400 hover:bg-blue-100 hover:text-blue-500"
             }
             `}
           >
             <div className="mr-2">
-              <HiOutlineHome className="h-5 w-5" />
+              <BiUserCheck className="h-5 w-5" />
             </div>
             <div>
               <p>AppliedUsers</p>
@@ -181,16 +182,16 @@ import { useNavigate } from "react-router-dom";
       <div className="flex flex-col ">
         <div onClick={() => router('/seekers')}>
           <div
-            className={`pl-7 py-3 mx-5 rounded text-center cursor-pointer mb-3 flex items-center transition-colors 
+            className={`pl-7 py-3 mx-5 rounded text-center cursor-pointer mb-1 flex items-center transition-colors 
             ${
               router.pathname === "/seekers"
                 ? "bg-blue-100 text-blue-500"
-                : "text-gray-400 hover:bg-blue-100 hover:text-blue-500"
+                : "text-black font-[600] hover:text-white  hover:bg-gradient-to-r from-sky-600 to-cyan-400 hover:bg-blue-100 hover:text-blue-500"
             }
             `}
           >
             <div className="mr-2">
-              <HiOutlineHome className="h-5 w-5" />
+              <RiUserSearchLine className="h-5 w-5" />
             </div>
             <div>
               <p>Seekers</p>
@@ -201,16 +202,16 @@ import { useNavigate } from "react-router-dom";
       <div className="flex flex-col ">
         <div onClick={() => router('/transactions')}>
           <div
-            className={`pl-7 py-3 mx-5 rounded text-center cursor-pointer mb-3 flex items-center transition-colors 
+            className={`pl-7 py-3 mx-5 rounded text-center cursor-pointer mb-1 flex items-center transition-colors 
             ${
               router.pathname === "/transactions"
                 ? "bg-blue-100 text-blue-500"
-                : "text-gray-400 hover:bg-blue-100 hover:text-blue-500"
+                : "text-black font-[600] hover:text-white  hover:bg-gradient-to-r from-sky-600 to-cyan-400 hover:bg-blue-100 hover:text-blue-500"
             }
             `}
           >
             <div className="mr-2">
-              <HiOutlineHome className="h-5 w-5" />
+              <AiOutlineTransaction className="h-5 w-5" />
             </div>
             <div>
               <p>transactions</p>
@@ -221,16 +222,16 @@ import { useNavigate } from "react-router-dom";
       <div className="flex flex-col ">
         <div onClick={() => router('/reports')}>
           <div
-            className={`pl-7 py-3 mx-5 rounded text-center cursor-pointer mb-3 flex items-center transition-colors 
+            className={`pl-7 py-3 mx-5 rounded text-center cursor-pointer mb-1 flex items-center transition-colors 
             ${
               router.pathname === "/reports"
                 ? "bg-blue-100 text-blue-500"
-                : "text-gray-400 hover:bg-blue-100 hover:text-blue-500"
+                : "text-black font-[600] hover:text-white  hover:bg-gradient-to-r from-sky-600 to-cyan-400 hover:bg-blue-100 hover:text-blue-500"
             }
             `}
           >
             <div className="mr-2">
-              <HiOutlineHome className="h-5 w-5" />
+              <FaRegListAlt className="h-5 w-5" />
             </div>
             <div>
               <p>Reports</p>
@@ -241,16 +242,16 @@ import { useNavigate } from "react-router-dom";
       <div className="flex flex-col ">
         <div onClick={() => router('/users')}>
           <div
-            className={`pl-7 py-3 mx-5 rounded text-center cursor-pointer mb-3 flex items-center transition-colors 
+            className={`pl-7 py-3 mx-5 rounded text-center cursor-pointer mb-1 flex items-center transition-colors 
             ${
               router.pathname === "/users"
                 ? "bg-blue-100 text-blue-500"
-                : "text-gray-400 hover:bg-blue-100 hover:text-blue-500"
+                : "text-black font-[600] hover:text-white  hover:bg-gradient-to-r from-sky-600 to-cyan-400 hover:bg-blue-100 hover:text-blue-500"
             }
             `}
           >
             <div className="mr-2">
-              <HiOutlineHome className="h-5 w-5" />
+              <HiOutlineUsers className="h-5 w-5" />
             </div>
             <div>
               <p>Users</p>
@@ -261,16 +262,16 @@ import { useNavigate } from "react-router-dom";
       <div className="flex flex-col ">
         <div onClick={() => router('/plans')}>
           <div
-            className={`pl-7 py-3 mx-5 rounded text-center cursor-pointer mb-3 flex items-center transition-colors 
+            className={`pl-7 py-3 mx-5 rounded text-center cursor-pointer mb-1 flex items-center transition-colors 
             ${
               router.pathname === "/plans"
                 ? "bg-blue-100 text-blue-500"
-                : "text-gray-400 hover:bg-blue-100 hover:text-blue-500"
+                : "text-black font-[600] hover:text-white  hover:bg-gradient-to-r from-sky-600 to-cyan-400 hover:bg-blue-100 hover:text-blue-500"
             }
             `}
           >
             <div className="mr-2">
-              <HiOutlineHome className="h-5 w-5" />
+              <AiOutlineOrderedList className="h-5 w-5" />
             </div>
             <div>
               <p>Plans</p>
@@ -281,11 +282,11 @@ import { useNavigate } from "react-router-dom";
       <div className="flex flex-col ">
         <div onClick={() => router('/payment')}>
           <div
-            className={`pl-7 py-3 mx-5 rounded text-center cursor-pointer mb-3 flex items-center transition-colors 
+            className={`pl-7 py-3 mx-5 rounded text-center cursor-pointer mb-1 flex items-center transition-colors 
             ${
               router.pathname === "/payment"
                 ? "bg-blue-100 text-blue-500"
-                : "text-gray-400 hover:bg-blue-100 hover:text-blue-500"
+                : "text-black font-[600] hover:text-white  hover:bg-gradient-to-r from-sky-600 to-cyan-400 hover:bg-blue-100 hover:text-blue-500"
             }
             `}
           >
@@ -301,16 +302,16 @@ import { useNavigate } from "react-router-dom";
       <div className="flex flex-col ">
         <div onClick={() => router('/notifications')}>
           <div
-            className={`pl-7 py-3 mx-5 rounded text-center cursor-pointer mb-3 flex items-center transition-colors 
+            className={`pl-7 py-3 mx-5 rounded text-center cursor-pointer mb-1 flex items-center transition-colors 
             ${
               router.pathname === "/notifications"
                 ? "bg-blue-100 text-blue-500"
-                : "text-gray-400 hover:bg-blue-100 hover:text-blue-500"
+                : "text-black font-[600] hover:text-white  hover:bg-gradient-to-r from-sky-600 to-cyan-400 hover:bg-blue-100 hover:text-blue-500"
             }
             `}
           >
             <div className="mr-2">
-              <HiOutlineHome className="h-5 w-5" />
+              <AiOutlineNotification className="h-5 w-5" />
             </div>
             <div>
               <p>Notifications</p>
