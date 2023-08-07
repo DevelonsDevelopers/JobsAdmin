90d268a3941d6909891428ea2bd221284823785a
AhmadHassan <AhmadHassan25202@gmail.com>
2023-08-07 09:55:19 -0700

    Sidebar Active Path Created

--- src/dashboard/Portal/Sidebar.jsx
+++ src/dashboard/Portal/Sidebar.jsx
@@ -2,34 +2,34 @@ import { forwardRef, useState } from "react";
 import { HiOutlineHome, HiOutlineUser, HiOutlineCblueitCard, HiOutlineUsers } from "react-icons/hi";
 
 import { RiEarthFill, RiEarthLine, RiHomeOfficeLine, RiPagesLine, RiUserSearchLine } from 'react-icons/ri'
-import { FaBoxes, FaPager, FaRegListAlt, FaTable, FaUser,FaUserCheck,FaUserClock } from 'react-icons/fa'
-import { BiCategoryAlt,BiUserCheck } from 'react-icons/bi'
+import { FaBoxes, FaPager, FaRegListAlt, FaTable, FaUser, FaUserCheck, FaUserClock } from 'react-icons/fa'
+import { BiCategoryAlt, BiUserCheck } from 'react-icons/bi'
 // import {FiUserCheck} from 'react-icons/fi'
 
-import { AiOutlineUserSwitch ,AiOutlineCloseCircle, AiOutlineTag, AiOutlineTransaction, AiOutlineOrderedList, AiOutlineNotification } from 'react-icons/ai'
+import { AiOutlineUserSwitch, AiOutlineCloseCircle, AiOutlineTag, AiOutlineTransaction, AiOutlineOrderedList, AiOutlineNotification } from 'react-icons/ai'
 import { PiBriefcaseLight, PiBuildingsLight, PiUsersThreeBold } from 'react-icons/pi'
 import { Button } from "@mui/material";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 
 
- const Sidebar = forwardRef(({ showNav }, ref) => {
+const Sidebar = forwardRef(({ showNav }, ref) => {
   const router = useNavigate();
-  const [active , isActive] = useState("false")
+  const location = useLocation();
+  const [active, isActive] = useState("false")
 
   return (
     <div ref={ref} className="mt-[-4rem] fixed w-56 h-full bg-white shadow-sm max-md:w-[35%] overflow-auto no-scrollbar">
-    
+
 
       <div className="flex flex-col mt-[2rem] ">
         <div onClick={() => router('/')}>
           <div
             className={`pl-7 py-3 mx-5 rounded text-center cursor-pointer mb-1 flex items-center transition-colors 
-            ${
-              router.pathname === "/"
-                ? "bg-blue-100 text-blue-500"
+            ${location.pathname === "/"
+                ? "bg-gradient-to-r from-sky-600 to-cyan-400 text-white"
                 : "text-black font-[600]  hover:text-white  hover:bg-gradient-to-r from-sky-600 to-cyan-400"
-            }
+              }
             `}
           >
             <div className="mr-2">
@@ -44,11 +44,12 @@ import { useNavigate } from "react-router-dom";
       <div className="flex flex-col ">
         <div onClick={() => router('/categories')}>
           <div
-          
-            // className={`px-2 py-2.5 hover:bg-cprimary-300 hover:text-csecond-100 rounded-md transition  `}
             className={`pl-7 py-3 mx-5 rounded text-center cursor-pointer mb-1 flex items-center transition-colors 
-            ${isActive && router === "/categories" ? "bg-blue-100 text-blue-500": "text-black font-[600] hover:text-white  hover:bg-gradient-to-r from-sky-600 to-cyan-400 hover:bg-blue-100 hover:text-blue-500"}
-            `}
+          ${location.pathname === "/categories"
+                ? "bg-gradient-to-r from-sky-600 to-cyan-400 text-white"
+                : "text-black font-[600]  hover:text-white  hover:bg-gradient-to-r from-sky-600 to-cyan-400"
+              }
+          `}
           >
             <div className="mr-2">
               <BiCategoryAlt className="h-5 w-5" />
@@ -63,15 +64,14 @@ import { useNavigate } from "react-router-dom";
         <div onClick={() => router('/countries')}>
           <div
             className={`pl-7 py-3 mx-5 rounded text-center cursor-pointer mb-1 flex items-center transition-colors 
-            ${
-              router.pathname === "/countries"
-                ? "bg-blue-100 text-blue-500"
+            ${location.pathname === "/countries"
+                ? "bg-gradient-to-r from-sky-600 to-cyan-400 text-white"
                 : "text-black font-[600] hover:text-white  hover:bg-gradient-to-r from-sky-600 to-cyan-400 hover:bg-blue-100 hover:text-blue-500"
-            }
+              }
             `}
           >
             <div className="mr-2">
-            <RiEarthLine className="h-5 w-5" />
+              <RiEarthLine className="h-5 w-5" />
             </div>
             <div>
               <p>Countries</p>
@@ -83,11 +83,10 @@ import { useNavigate } from "react-router-dom";
         <div onClick={() => router('/cities')}>
           <div
             className={`pl-7 py-3 mx-5 rounded text-center cursor-pointer mb-1 flex items-center transition-colors 
-            ${
-              router.pathname === "/cities"
-                ? "bg-blue-100 text-blue-500"
+            ${location.pathname === "/cities"
+                ? "bg-gradient-to-r from-sky-600 to-cyan-400 text-white"
                 : "text-black font-[600] hover:text-white  hover:bg-gradient-to-r from-sky-600 to-cyan-400 hover:bg-blue-100 hover:text-blue-500"
-            }
+              }
             `}
           >
             <div className="mr-2">
@@ -103,11 +102,10 @@ import { useNavigate } from "react-router-dom";
         <div onClick={() => router('/jobs')}>
           <div
             className={`pl-7 py-3 mx-5 rounded text-center cursor-pointer mb-1 flex items-center transition-colors 
-            ${
-              router.pathname === "/jobs"
-                ? "bg-blue-100 text-blue-500"
+            ${location.pathname === "/jobs"
+                ? "bg-gradient-to-r from-sky-600 to-cyan-400 text-white"
                 : "text-black font-[600] hover:text-white  hover:bg-gradient-to-r from-sky-600 to-cyan-400 hover:bg-blue-100 hover:text-blue-500"
-            }
+              }
             `}
           >
             <div className="mr-2">
@@ -123,11 +121,10 @@ import { useNavigate } from "react-router-dom";
         <div onClick={() => router('/companies')}>
           <div
             className={`pl-7 py-3 mx-5 rounded text-center cursor-pointer mb-1 flex items-center transition-colors 
-            ${
-              router.pathname === "/companies"
-                ? "bg-blue-100 text-blue-500"
+            ${location.pathname === "/companies"
+                ? "bg-gradient-to-r from-sky-600 to-cyan-400 text-white"
                 : "text-black font-[600] hover:text-white  hover:bg-gradient-to-r from-sky-600 to-cyan-400 hover:bg-blue-100 hover:text-blue-500"
-            }
+              }
             `}
           >
             <div className="mr-2">
@@ -143,11 +140,10 @@ import { useNavigate } from "react-router-dom";
         <div onClick={() => router('/tags')}>
           <div
             className={`pl-7 py-3 mx-5 rounded text-center cursor-pointer mb-1 flex items-center transition-colors 
-            ${
-              router.pathname === "/tags"
-                ? "bg-blue-100 text-blue-500"
+            ${location.pathname === "/tags"
+                ? "bg-gradient-to-r from-sky-600 to-cyan-400 text-white"
                 : "text-black font-[600] hover:text-white  hover:bg-gradient-to-r from-sky-600 to-cyan-400 hover:bg-blue-100 hover:text-blue-500"
-            }
+              }
             `}
           >
             <div className="mr-2">
@@ -163,11 +159,10 @@ import { useNavigate } from "react-router-dom";
         <div onClick={() => router('/appliedusers')}>
           <div
             className={`pl-7 py-3 mx-5 rounded text-center cursor-pointer mb-1 flex items-center transition-colors 
-            ${
-              router.pathname === "/appliedusers"
-                ? "bg-blue-100 text-blue-500"
+            ${location.pathname === "/appliedusers"
+                ? "bg-gradient-to-r from-sky-600 to-cyan-400 text-white"
                 : "text-black font-[600] hover:text-white  hover:bg-gradient-to-r from-sky-600 to-cyan-400 hover:bg-blue-100 hover:text-blue-500"
-            }
+              }
             `}
           >
             <div className="mr-2">
@@ -183,11 +178,10 @@ import { useNavigate } from "react-router-dom";
         <div onClick={() => router('/seekers')}>
           <div
             className={`pl-7 py-3 mx-5 rounded text-center cursor-pointer mb-1 flex items-center transition-colors 
-            ${
-              router.pathname === "/seekers"
-                ? "bg-blue-100 text-blue-500"
+            ${location.pathname === "/seekers"
+                ? "bg-gradient-to-r from-sky-600 to-cyan-400 text-white"
                 : "text-black font-[600] hover:text-white  hover:bg-gradient-to-r from-sky-600 to-cyan-400 hover:bg-blue-100 hover:text-blue-500"
-            }
+              }
             `}
           >
             <div className="mr-2">
@@ -203,11 +197,10 @@ import { useNavigate } from "react-router-dom";
         <div onClick={() => router('/transactions')}>
           <div
             className={`pl-7 py-3 mx-5 rounded text-center cursor-pointer mb-1 flex items-center transition-colors 
-            ${
-              router.pathname === "/transactions"
-                ? "bg-blue-100 text-blue-500"
+            ${location.pathname === "/transactions"
+                ? "bg-gradient-to-r from-sky-600 to-cyan-400 text-white"
                 : "text-black font-[600] hover:text-white  hover:bg-gradient-to-r from-sky-600 to-cyan-400 hover:bg-blue-100 hover:text-blue-500"
-            }
+              }
             `}
           >
             <div className="mr-2">
@@ -223,11 +216,10 @@ import { useNavigate } from "react-router-dom";
         <div onClick={() => router('/reports')}>
           <div
             className={`pl-7 py-3 mx-5 rounded text-center cursor-pointer mb-1 flex items-center transition-colors 
-            ${
-              router.pathname === "/reports"
-                ? "bg-blue-100 text-blue-500"
+            ${location.pathname === "/reports"
+                ? "bg-gradient-to-r from-sky-600 to-cyan-400 text-white"
                 : "text-black font-[600] hover:text-white  hover:bg-gradient-to-r from-sky-600 to-cyan-400 hover:bg-blue-100 hover:text-blue-500"
-            }
+              }
             `}
           >
             <div className="mr-2">
@@ -243,11 +235,10 @@ import { useNavigate } from "react-router-dom";
         <div onClick={() => router('/users')}>
           <div
             className={`pl-7 py-3 mx-5 rounded text-center cursor-pointer mb-1 flex items-center transition-colors 
-            ${
-              router.pathname === "/users"
-                ? "bg-blue-100 text-blue-500"
+            ${location.pathname === "/users"
+                ? "bg-gradient-to-r from-sky-600 to-cyan-400 text-white"
                 : "text-black font-[600] hover:text-white  hover:bg-gradient-to-r from-sky-600 to-cyan-400 hover:bg-blue-100 hover:text-blue-500"
-            }
+              }
             `}
           >
             <div className="mr-2">
@@ -263,11 +254,10 @@ import { useNavigate } from "react-router-dom";
         <div onClick={() => router('/plans')}>
           <div
             className={`pl-7 py-3 mx-5 rounded text-center cursor-pointer mb-1 flex items-center transition-colors 
-            ${
-              router.pathname === "/plans"
-                ? "bg-blue-100 text-blue-500"
+            ${location.pathname === "/plans"
+                ? "bg-gradient-to-r from-sky-600 to-cyan-400 text-white"
                 : "text-black font-[600] hover:text-white  hover:bg-gradient-to-r from-sky-600 to-cyan-400 hover:bg-blue-100 hover:text-blue-500"
-            }
+              }
             `}
           >
             <div className="mr-2">
@@ -283,11 +273,10 @@ import { useNavigate } from "react-router-dom";
         <div onClick={() => router('/payment')}>
           <div
             className={`pl-7 py-3 mx-5 rounded text-center cursor-pointer mb-1 flex items-center transition-colors 
-            ${
-              router.pathname === "/payment"
-                ? "bg-blue-100 text-blue-500"
+            ${location.pathname === "/payment"
+                ? "bg-gradient-to-r from-sky-600 to-cyan-400 text-white"
                 : "text-black font-[600] hover:text-white  hover:bg-gradient-to-r from-sky-600 to-cyan-400 hover:bg-blue-100 hover:text-blue-500"
-            }
+              }
             `}
           >
             <div className="mr-2">
@@ -303,11 +292,10 @@ import { useNavigate } from "react-router-dom";
         <div onClick={() => router('/notifications')}>
           <div
             className={`pl-7 py-3 mx-5 rounded text-center cursor-pointer mb-1 flex items-center transition-colors 
-            ${
-              router.pathname === "/notifications"
-                ? "bg-blue-100 text-blue-500"
+            ${location.pathname === "/notifications"
+                ? "bg-gradient-to-r from-sky-600 to-cyan-400 text-white"
                 : "text-black font-[600] hover:text-white  hover:bg-gradient-to-r from-sky-600 to-cyan-400 hover:bg-blue-100 hover:text-blue-500"
-            }
+              }
             `}
           >
             <div className="mr-2">
