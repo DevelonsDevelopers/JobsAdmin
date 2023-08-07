ad8e10068c2ce1eef27cea573b729c4a9647e9c3
username880 <abubakar.4983763@gmail.com>
2023-08-07 08:57:59 -0700

    testing 2

--- src/components/ViewModal.jsx
+++ src/components/ViewModal.jsx
@@ -1,31 +1,36 @@
-import { Button, Dialog, DialogActions, DialogContent, DialogTitle, Modal, Typography } from '@mui/material'
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Modal, Typography } from '@mui/material'
 import React, { useState } from 'react'
 
-const ViewModal = ({open, setOpen,title,data}) => {
-    // const [open, setOpen] = useState(false)
-    // const [openSnackbar, closeSnackbar] = useSnackbar('Customer Deleted Successfully');
-    
-    const handleClose = () => {
-        setOpen(!open)
-    }
+const ViewModal = ({ open, setOpen, title, data }) => {
+
+  const handleClose = () => {
+    setOpen(!open)
+  }
 
   return (
     <Dialog className='w-[100%] p-[5%] rounded-lg' onClose={handleClose} aria-labelledby="customized-dialog-title" PaperProps={{ style: { borderRadius: 20, padding: 15 } }} open={open}>
-        <DialogTitle id="customized-dialog-title" onClose={handleClose} className='w-[30rem] flex px-[10%]'>
-            <div classsName='font-900 '>{title}</div>
-        </DialogTitle>
-        <DialogContent className='my-[4%] mx-[10%] flex gap-5 items-center'>
-            <h1 className='pt-[4%] '>{data?.name}</h1>
-            <p className='pt-[4%] '>{data?.count}</p>
-        </DialogContent>
-        <center>
-            <div className='flex items-center  px-[10%] pb-[5%]  flex-row-reverse'>
-            <button variant="text" className='text-black ' onClick={handleClose}>
-              Cancel
-            </button>
-            
-          </div>
-          </center>
+      <DialogTitle id="customized-dialog-title" onClose={handleClose} className='!m-auto'>
+        {title}
+      </DialogTitle>
+   <hr />
+      <DialogContent className='my-[4%] mx-[10%] items-center'>
+        <p>  City Name:  </p>
+        <hr />
+        <p >{data?.name}</p>
+        <DialogContentText>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, nulla.</DialogContentText>
+        <p className='pt-4'> Total Jobs Count: </p>
+        <hr />
+        <p >{data?.count}</p>
+        <DialogContentText>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, nulla.</DialogContentText>
+      </DialogContent>
+      <hr />
+        <div className='!m-auto pb-5 pt-5 '>
+          <button variant="text" className='text-white bg-blue-500 p-2 rounded-xl ' onClick={handleClose}>
+            Cancel
+          </button>
+
+        </div>
+    
     </Dialog>
   )
 }
