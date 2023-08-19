7c5dee63560b7db339683270e61b3f969daab9b9
AhmadHassan <AhmadHassan25202@gmail.com>
2023-08-19 10:58:42 -0700

    Add and delete Created

--- src/store/actions/cityActions.js
+++ src/store/actions/cityActions.js
@@ -1,4 +1,4 @@
-import {  ALL_CITIES, LOADING, SUCCESS } from "../../Utils/Constant"
+import {  ALL_CITIES, CREATE_CITY, DELETE_CITY, GET_CITY, LOADING, SUCCESS, UPDATE_CITY } from "../../Utils/Constant"
 import * as api from '../../store/index'
 
 
@@ -11,5 +11,42 @@ export const AllCities = () => async (dispatch) => {
         dispatch({ type: SUCCESS })
     } catch(error) {
         console.log(error)
+    }
+}
+
+export const getCity = (id) => async (dispatch) => {
+    try{
+        const { data } = await api.fetchCity(id)
+        dispatch({ type: GET_CITY, payload: data})
+    } catch(error){
+        console.log(error)
+    }
+}
+
+export const createCity = (city) => async (disptach) =>{
+    try{
+        const {data: {data}} = await api.createCity(city)
+        disptach({ type: CREATE_CITY, payload: data})
+    }catch(error){
+        console.log(error)
+    }
+}
+
+export const updateCity = (id , city) => async (dispatch) => {
+    try{
+        const {data: {data} } = await api.updateCity(id, city)
+        dispatch({ type: UPDATE_CITY, payload: data})
+    }catch(error){
+        console.log(error)
+    }
+}
+
+export const DeleteCity = (id) => async (dispatch) => {
+    try {
+        const { data } = await api.deleteCity(id)
+        console.log(id)
+        dispatch({ type: DELETE_CITY, payload: id})
+    } catch(error) {
+        console.log(error);
     }
 }
--- src/store/reducers/user.js
+++ src/store/reducers/user.js
@@ -1,16 +1,24 @@
-import {   ALL_USERS, ERROR, LOADING, SUCCESS } from "../../Utils/Constant"
+import { ALL_USERS, CREATE_USER, DELETE_USER, ERROR, GET_USER, LOADING, SUCCESS, UPDATE_USER } from "../../Utils/Constant"
 
 
-const user = (state = {isLoading: true, success: false, error: false, users: []}, action) =>{
-    switch(action.type){
+const user = (state = { isLoading: true, success: false, error: false, users: [] }, action) => {
+    switch (action.type) {
         case LOADING:
-            return {...state, isLoading: true}
+            return { ...state, isLoading: true }
         case SUCCESS:
-            return {...state, isLoading: false, success: true, error: false}
+            return { ...state, isLoading: false, success: true, error: false }
         case ERROR:
-            return {...state, isLoading: false, success: false, error: true}
+            return { ...state, isLoading: false, success: false, error: true }
         case ALL_USERS:
-            return {...state, users: action.payload.users}
+            return { ...state, users: action.payload.users }
+        case GET_USER:
+            return { ...state, user: action.payload }
+        case CREATE_USER:
+            return { ...state, users: [...state.users, action.payload] }
+        case UPDATE_USER:
+            return { ...state, users: state.users.map((user) => (user.id === action.payload.id ? action.payload : user)) }
+        case DELETE_USER:
+            return { ...state, users: state.users.filter((user) => user.id !== action.payload) }
         default:
             return state;
 
