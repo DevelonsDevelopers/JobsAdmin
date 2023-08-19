0754be5e3679125a61c7ec337fc7de283c687350
AhmadHassan <AhmadHassan25202@gmail.com>
2023-08-19 07:40:13 -0700

    test

--- src/dashboard/Countries.jsx
+++ src/dashboard/Countries.jsx
@@ -79,7 +79,9 @@ const Countries = () => {
 
   <div className='flex justify-center mt-[3rem] w-[90%] m-auto'>
 
-    <input type="search"  onChange={(e) => setSearch(e.target.value)}  name="" id="" placeholder='Search...' className='border-2 border-gray-600 pl-[4rem] rounded-[1.0625rem] py-2  w-[27.8125rem] mr-auto max-md:py-[1px] max-md:w-[15rem] max-md:text-[0.7rem] focus:outline-none focus:ring-0 focus:border-gray-900 peer' />
+    <input type="search"  
+    // onChange={(e) => setSearch(e.target.value)} 
+     name="" id="" placeholder='Search...' className='border-2 border-gray-600 pl-[4rem] rounded-[1.0625rem] py-2  w-[27.8125rem] mr-auto max-md:py-[1px] max-md:w-[15rem] max-md:text-[0.7rem] focus:outline-none focus:ring-0 focus:border-gray-900 peer' />
     <Link  to="/countries/add"> <button className="bg-[#0047FF] cursor-pointer  max-md:text-[.6rem] py-2 px-[1rem] max-md:px-[1rem] max-md:py-[5px] text-white font-[500] max-md:font-[400] rounded-[1.375rem] ml-auto "  >
       Add New
     </button>
@@ -103,10 +105,12 @@ const Countries = () => {
 
           </thead>
 
-          {countries.filter((value)=>{
-            return search.toLowerCase() === ''
-            ? value :value.name.toLowerCase().includes(search);
-          }).map((value, index) => (
+          {countries
+          // .filter((value)=>{
+          //   return search.toLowerCase() === ''
+          //   ? value :value.name.toLowerCase().includes(search);
+          // })
+          .map((value, index) => (
             <tbody className="text-[#000000] text-sm font-light w-[100%] bg-white ">
               <tr className='' >
                 <td className="py-[2%] w-[3%]   border-r-[1px] border-t-[1px]   text-center">
