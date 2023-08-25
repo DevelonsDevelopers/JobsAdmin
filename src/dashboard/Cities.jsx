c0ebfedf26ef125f92ec4623b7cf1e6ceacdc6fe
username880 <abubakar.4983763@gmail.com>
2023-08-25 04:21:29 -0700

    cities edit update

--- src/dashboard/Cities.jsx
+++ src/dashboard/Cities.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import PortalLayout from '../portalLayout/PortalLayout'
 import DeleteModal from '../components/DeleteModal'
-import { useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import ViewModal from '../components/ViewModal'
 import { useDispatch, useSelector } from 'react-redux'
 import { AllCities, DeleteCity } from '../store/actions/cityActions'
@@ -69,10 +69,10 @@ const Cities = () => {
         <div className='flex justify-center mt-[3rem] w-[90%] m-auto'>
 
           <input type="search" name="" id="" placeholder='Search...' className='border-2 border-gray-600 pl-[4rem] rounded-[1.0625rem] py-2  w-[27.8125rem] mr-auto max-md:py-[1px] max-md:w-[15rem] max-md:text-[0.7rem]' />
-          <a href="/cities/add"> <button className="bg-[#0047FF] cursor-pointer  max-md:text-[.6rem] py-2 px-[1rem] max-md:px-[1rem] max-md:py-[5px] text-white font-[600] max-md:font-[400] rounded-[1.375rem] ml-auto "  >
+          <Link  to="/cities/add"> <button className="bg-[#0047FF] cursor-pointer  max-md:text-[.6rem] py-2 px-[1rem] max-md:px-[1rem] max-md:py-[5px] text-white font-[600] max-md:font-[400] rounded-[1.375rem] ml-auto "  >
             Add New
           </button>
-          </a>
+          </Link>
 
         </div>
         <DeleteModal open={open} setOpen={setOpen} ID={deleteId} deleteFunction={deleteCity} />
