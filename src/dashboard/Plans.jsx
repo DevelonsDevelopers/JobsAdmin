2bd8765256dfd4f435905b2157e30b9624d9c594
username880 <abubakar.4983763@gmail.com>
2023-08-25 04:29:06 -0700

    tags applieduser user plan edit update

--- src/dashboard/Plans.jsx
+++ src/dashboard/Plans.jsx
@@ -9,11 +9,11 @@ import { AllPlans } from '../store/actions/planActions';
 
 
 const Plans = () => {
-// search===============
-const [search ,setSearch  ] = useState('')
-console.log(search)
-// =============
- 
+  // search===============
+  const [search, setSearch] = useState('')
+  console.log(search)
+  // =============
+
 
 
 
@@ -21,12 +21,12 @@ console.log(search)
   const [open, setOpen] = useState(false);
   const [openView, setOpenView] = useState(false);
   const [view, setView] = useState(false);
-  const router  = useNavigate();
+  const router = useNavigate();
   const [data, setData] = useState()
 
   const dispatch = useDispatch();
 
-  
+
   const handleClick = (value) => {
     setOpenView(!open)
     setData(value)
@@ -40,26 +40,26 @@ console.log(search)
   useEffect(() => {
     dispatch(AllPlans())
   }, [dispatch])
-  
+
   return (
     <PortalLayout>
       <h1 className='text-[3.125rem] font-[800] text-[#000] text-center max-md:text-[2rem] uppercase'>Plans</h1>
 
-<div className="w-[100%] max-md:h-full  max-md:px-2 flex flex-col justify-center bg-gray-100">
+      <div className="w-[100%] max-md:h-full  max-md:px-2 flex flex-col justify-center bg-gray-100">
 
-  <div className='flex justify-center mt-[3rem] w-[90%] m-auto'>
+        <div className='flex justify-center mt-[3rem] w-[90%] m-auto'>
 
-    <input type="search" onChange={(e) => setSearch(e.target.value)}
-  name="" id="" placeholder='Search...' className='border-2 border-gray-600 pl-[4rem] rounded-[1.0625rem] py-2  w-[27.8125rem] mr-auto max-md:py-[1px] max-md:w-[15rem] max-md:text-[0.7rem] focus:outline-none focus:ring-0 focus:border-gray-900 peer' />
-    <Link href="/plans/add"> <button className="bg-[#0047FF] cursor-pointer  max-md:text-[.6rem] py-2 px-[1rem] max-md:px-[1rem] max-md:py-[5px] text-white font-[600] max-md:font-[400] rounded-[1.375rem] ml-auto "  >
-      Add New
-    </button>
-    </Link>
+          <input type="search" onChange={(e) => setSearch(e.target.value)}
+            name="" id="" placeholder='Search...' className='border-2 border-gray-600 pl-[4rem] rounded-[1.0625rem] py-2  w-[27.8125rem] mr-auto max-md:py-[1px] max-md:w-[15rem] max-md:text-[0.7rem] focus:outline-none focus:ring-0 focus:border-gray-900 peer' />
+          <Link to="/plans/add"> <button className="bg-[#0047FF] cursor-pointer  max-md:text-[.6rem] py-2 px-[1rem] max-md:px-[1rem] max-md:py-[5px] text-white font-[600] max-md:font-[400] rounded-[1.375rem] ml-auto "  >
+            Add New
+          </button>
+          </Link>
 
-  </div>
-  <DeleteModal open={open} setOpen={setOpen} />
-    <PlansView open={openView} setOpen={setOpenView} title={" VIEW"} data={data} />
-    <div className="rounded-xl p-5 bg-white w-[90%] m-auto max-md:w-[100%]  mt-6 max-md:overflow-auto ">
+        </div>
+        <DeleteModal open={open} setOpen={setOpen} />
+        <PlansView open={openView} setOpen={setOpenView} title={" VIEW"} data={data} />
+        <div className="rounded-xl p-5 bg-white w-[90%] m-auto max-md:w-[100%]  mt-6 max-md:overflow-auto ">
           <thead className='mt-10'>
 
             <tr className=" uppercase  text-sm leading-normal w-[100%]">
@@ -68,7 +68,7 @@ console.log(search)
               <th className="py-[2%] border-r-[1px] border-b-[2px] border-b-black  w-[3%] max-md:text-[.6rem] max-md:font-[400] text-center max-md:w-[2%]  text-[13px]">Amount </th>
               <th className="py-[2%] border-r-[1px] border-b-[2px] border-b-black  w-[3%] max-md:text-[.6rem] max-md:font-[400] text-center max-md:w-[2%]  text-[13px]">Usertype </th>
               <th className="py-[2%] border-r-[1px] border-b-[2px] border-b-black  w-[3%] max-md:text-[.6rem] max-md:font-[400] text-center max-md:w-[2%]  text-[13px]">Purpose </th>
-             
+
               <th className="py-[2%] border-r-[1px] border-b-[2px] border-b-black w-[1%] max-md:text-[.6rem] max-md:font-[400] text-center max-md:w-[3%] text-[13px]">TimePeriod</th>
               <th className="py-[2%] border-r-[1px] border-b-[2px] border-b-black w-[2%] max-md:text-[.6rem] max-md:font-[400] text-center text-[13px]">Status</th>
               <th className="py-[2%] border-r-[1px] border-b-[2px] border-b-black  w-[2%] max-md:text-[.6rem] max-md:font-[400] text-center text-[13px]">Actions</th>
@@ -78,68 +78,68 @@ console.log(search)
 
           </thead>
 
-          {plans.filter((value)=>{
+          {plans.filter((value) => {
             return search.toLowerCase() === ''
-            ? value :value.name.toLowerCase().includes(search);
+              ? value : value.name.toLowerCase().includes(search);
           })
-          .map((value, index) => (
-            <tbody className="text-[#000000] text-sm font-light w-[100%] bg-white ">
-              <tr className='' >
-                <td className="py-[2%] w-[1%]   border-r-[1px] border-t-[1px]   text-center">
-                  <span className="font-bold max-md:text-[.7rem] text-[13px] text-blue-500">{value.id}</span>
-                </td>
-                <td className="py-[2%] w-[2%]   border-r-[1px] border-t-[1px]   text-center">
-                  <span className="font-bold max-md:text-[.7rem] text-[13px] font-[300] ">{value.name}</span>
-                </td>
-                <td className="py-[2%] w-[3%]   border-r-[1px] border-t-[1px]   text-center">
-                  <span className="font-bold max-md:text-[.7rem] text-[13px] font-[300] ">{value.amount}</span>
-                </td>
-                <td className="py-[2%] w-[3%]   border-r-[1px] border-t-[1px]   text-center">
-                  <span className="font-bold max-md:text-[.7rem] text-[13px] font-[300] ">{value.type}</span>
-                </td>
-                <td className="py-[2%] w-[3%]   border-r-[1px] border-t-[1px]   text-center">
-                  <span className="font-bold max-md:text-[.7rem] text-[13px] font-[300] ">{value.purpose}</span>
-                </td>
-                
-                <td className="py-[1%] w-[2%]  max-md:text-[.7rem]  border-r-[1px] border-t-[1px] text-center">
-                  <span className='text-[13px] font-[350]'>{value.timeperiod}</span>
-                </td>
+            .map((value, index) => (
+              <tbody className="text-[#000000] text-sm font-light w-[100%] bg-white ">
+                <tr className='' >
+                  <td className="py-[2%] w-[1%]   border-r-[1px] border-t-[1px]   text-center">
+                    <span className="font-bold max-md:text-[.7rem] text-[13px] text-blue-500">{value.id}</span>
+                  </td>
+                  <td className="py-[2%] w-[2%]   border-r-[1px] border-t-[1px]   text-center">
+                    <span className="font-bold max-md:text-[.7rem] text-[13px] font-[300] ">{value.name}</span>
+                  </td>
+                  <td className="py-[2%] w-[3%]   border-r-[1px] border-t-[1px]   text-center">
+                    <span className="font-bold max-md:text-[.7rem] text-[13px] font-[300] ">{value.amount}</span>
+                  </td>
+                  <td className="py-[2%] w-[3%]   border-r-[1px] border-t-[1px]   text-center">
+                    <span className="font-bold max-md:text-[.7rem] text-[13px] font-[300] ">{value.type}</span>
+                  </td>
+                  <td className="py-[2%] w-[3%]   border-r-[1px] border-t-[1px]   text-center">
+                    <span className="font-bold max-md:text-[.7rem] text-[13px] font-[300] ">{value.purpose}</span>
+                  </td>
 
-                <td className="py-[2%] max-md:text-[.7rem] w-[2%] border-r-[1px] border-t-[1px]   text-center">
-                  <span className="bg-green-600 text-white font-[500] py-[3px] px-[10px] max-md:w-[8%] rounded-xl text-[0.6rem] max-md:py-1 max-md:px-2 max-md:text-[0.6rem] cursor-pointer hover:bg-green-700 ">{value.status}</span>
-                </td>
-                <td className="py-[2%] w-[2%] max-md:text-[.7rem]  border-r-[1px] border-t-[1px]   text-center">
-                  <div className="flex item-center justify-center gap-3">
+                  <td className="py-[1%] w-[2%]  max-md:text-[.7rem]  border-r-[1px] border-t-[1px] text-center">
+                    <span className='text-[13px] font-[350]'>{value.timeperiod}</span>
+                  </td>
 
-                    <div className="w-4 mr-2 transform hover:text-blue-500  hover:scale-110" onClick={() => router("/categories/edit")}>
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="blue">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
-                      </svg>
+                  <td className="py-[2%] max-md:text-[.7rem] w-[2%] border-r-[1px] border-t-[1px]   text-center">
+                    <span className="bg-green-600 text-white font-[500] py-[3px] px-[10px] max-md:w-[8%] rounded-xl text-[0.6rem] max-md:py-1 max-md:px-2 max-md:text-[0.6rem] cursor-pointer hover:bg-green-700 ">{value.status}</span>
+                  </td>
+                  <td className="py-[2%] w-[2%] max-md:text-[.7rem]  border-r-[1px] border-t-[1px]   text-center">
+                    <div className="flex item-center justify-center gap-3">
+
+                      <div className="w-4 mr-2 transform hover:text-blue-500  hover:scale-110" onClick={() => router("/plans/edit")}>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="blue">
+                          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
+                        </svg>
+                      </div>
+                      <div className="w-4 mr-2 transform hover:text-blue-500  hover:scale-110" onClick={() => setOpen(true)}>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="red">
+                          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                        </svg>
+                      </div>
                     </div>
-                    <div className="w-4 mr-2 transform hover:text-blue-500  hover:scale-110" onClick={() => setOpen(true)}>
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="red">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-                      </svg>
+                  </td>
+
+                  <td className="py-[2%] w-[1%] max-md:text-[.7rem]  border-t-[1px]   ">
+                    <div className="w-4 m-auto transform hover:text-blue-500  hover:scale-110 " onClick={() => handleClick(value)}>   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" >
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+                    </svg>
                     </div>
-                  </div>
-                </td>
+                  </td>
+                </tr>
 
-                <td className="py-[2%] w-[1%] max-md:text-[.7rem]  border-t-[1px]   ">
-                  <div className="w-4 m-auto transform hover:text-blue-500  hover:scale-110 " onClick={() => handleClick(value)}>   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" >
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
-                  </svg>
-                  </div>
-                </td>
-              </tr>
-
-            </tbody>
-          ))}
+              </tbody>
+            ))}
         </div>
 
-  <center>
-  </center>
-</div>
+        <center>
+        </center>
+      </div>
     </PortalLayout>
   )
 }
--- src/dashboard/edit/UsersEdit.jsx
+++ src/dashboard/edit/UsersEdit.jsx
@@ -5,43 +5,59 @@ import PortalLayout from '../../portalLayout/PortalLayout'
 const UsersEdit = () => {
     return (
         <PortalLayout>
-            <h1 className='text-center bg-blue-600 text-white font-[600] mb-5 py-4 rounded-xl shadow-md shadow-blue-300 text-[1.5rem]'>EDIT USERS</h1>
-            <div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col my-2">
+            <h1 className='text-center bg-gradient-to-r from-sky-600 to-cyan-400 text-white font-[600] mb-5 py-4 rounded-xl shadow-md shadow-blue-300 text-[1.5rem]'>EDIT USERS</h1>
+            <div class="bg-white shadow-md rounded px-[10rem] max-md:px-4 pt-6 pb-8 mb-4 flex flex-col my-2">
                 <form action="">
-                    <div class="-mx-3 md:flex mb-6">
-                        <div class="md:w-1/2 px-3 mb-6 md:mb-0">
-                            <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2" for="grid-first-name">
-                                USERNAME
-                            </label>
-                            <input type="text" name="floating_email" id="floating_email" className="pl-4 block py-2.5 px-0 w-full text-md text-gray-900 bg-transparent rounded-xl p-2 border-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="Enter UserName" required />
-                        </div>
-                        <div class="md:w-1/2 px-3">
-                            <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2" for="grid-last-name">
-                                EMAIL
-                            </label>
-                            <input type="text" name="floating_email" id="floating_email" className="pl-4 block py-2.5 px-0 w-full text-md text-gray-900 bg-transparent rounded-xl p-2 border-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="Enter Email" required />
-                        </div>
-                    </div>
-                    <div class="-mx-3 md:flex mb-6">
-                        <div class="md:w-1/2 px-3 mb-6 md:mb-0">
-                            <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2" for="grid-first-name">
-                                PASSWORD
-                            </label>
-                            <input type="text" name="floating_email" id="floating_email" className="pl-4 block py-2.5 px-0 w-full text-md text-gray-900 bg-transparent rounded-xl p-2 border-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="Enter Password" required />
-                        </div>
-                        <div class="md:w-1/2 px-3">
-                            <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2" for="grid-last-name">
-                                PHONE NO
-                            </label>
-                            <input type="text" name="floating_email" id="floating_email" className="pl-4 block py-2.5 px-0 w-full text-md text-gray-900 bg-transparent rounded-xl p-2 border-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="Enter Phone Number" required />
-                        </div>
-                    </div>
+                <div className="-mx-3 mt-[-1.2rem] mb-6">
+          <div className="w-[100%] px-3 mb-6 md:mb-0 mt-5">
+            <label className="block  tracking-wide text-grey-darker text-[0.7rem] font-[600] mb-[3px] ml-4" for="grid-first-name">
+              Name
+            </label>
+            <input type="text" name="floating_email" id="floating_email"  className="pl-4 block py-[9px] px-0 w-full text-sm text-gray-900 bg-gray-50 rounded-[9px] border-[0.7px] border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="Enter Company Name" required />
+          </div>
+        </div>
+        <div className='grid grid-cols-2 gap-10 mt-10'>
+          <div className="-mx-3 mt-[-1.2rem] mb-6">
+            <div className="w-[100%] px-3 mb-6 md:mb-0">
+              <label className="block  tracking-wide text-grey-darker text-[0.7rem] font-[600] mb-[3px] ml-4" for="grid-first-name">
+                User Name
+              </label>
+              <input type="text" name="floating_email" id="floating_email"  className="pl-4 block py-[9px] px-0 w-full text-sm text-gray-900 bg-gray-50 rounded-[9px] border-[0.7px] border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="Enter User Name" required />
+            </div>
+          </div>
+          <div className="-mx-3 mt-[-1.2rem] mb-6">
+            <div className="w-[100%] px-3 mb-6 md:mb-0">
+              <label className="block  tracking-wide text-grey-darker text-[0.7rem] font-[600] mb-[3px] ml-4" for="grid-first-name">
+                Email
+              </label>
+              <input type="email" name="floating_email" id="floating_email"  className="pl-4 block py-[9px] px-0 w-full text-sm text-gray-900 bg-gray-50 rounded-[9px] border-[0.7px] border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="Enter  Email " required />
+            </div>
+          </div>
+        </div>
+        <div className='grid grid-cols-2 gap-10 mt-5'>
+          <div className="-mx-3 mt-[-1.2rem] mb-6">
+            <div className="w-[100%] px-3 mb-6 md:mb-0">
+              <label className="block  tracking-wide text-grey-darker text-[0.7rem] font-[600] mb-[3px] ml-4" for="grid-first-name">
+                Password
+              </label>
+              <input type="password" name="floating_email" id="floating_email"  className="pl-4 block py-[9px] px-0 w-full text-sm text-gray-900 bg-gray-50 rounded-[9px] border-[0.7px] border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="Enter Password" required />
+            </div>
+          </div>
+          <div className="-mx-3 mt-[-1.2rem] mb-6">
+            <div className="w-[100%] px-3 mb-6 md:mb-0">
+              <label className="block  tracking-wide text-grey-darker text-[0.7rem] font-[600] mb-[3px] ml-4" for="grid-first-name">
+                Phone
+              </label>
+              <input type="email" name="floating_email" id="floating_email"  className="pl-4 block py-[9px] px-0 w-full text-sm text-gray-900 bg-gray-50 rounded-[9px] border-[0.7px] border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="Enter Contact Number " required />
+            </div>
+          </div>
+        </div>
                     <div class="-mx-3 md:flex mb-6">
                         <div class="md:w-full px-3">
-                            <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2" for="grid-password">
-                                ADDRESS
-                            </label>
-                            <input type="text" name="floating_email" id="floating_email" className="pl-4 block py-2.5 px-0 w-full text-md text-gray-900 bg-transparent rounded-xl p-2 border-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="Enter Address" required />
+                        <label className="block  tracking-wide text-grey-darker text-[0.7rem] font-[600] mb-[3px] ml-4" for="grid-first-name">
+                Address
+              </label>
+                            <input  type="text" name="address" id="floating_email" className="pl-4 block py-[9px] px-0 w-full text-sm text-gray-900 bg-gray-50 rounded-[9px] border-[0.7px] border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="Enter Address" required />
                         </div>
                     </div>
                     <div className='flex justify-center'>
