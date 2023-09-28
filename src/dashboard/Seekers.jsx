32934e6853932c0da43facbb846718344c3d2ef3
username880 <abubakar.4983763@gmail.com>
2023-09-28 15:55:06 -0700

    login page pagination

--- src/dashboard/Seekers.jsx
+++ src/dashboard/Seekers.jsx
@@ -187,14 +187,14 @@ console.log(seekers)
                 <nav className='m-auto mt-5' >
                   <ul className="flex items-center -space-x-px h-10 text-base">
                     <li>
-                      <a onClick={prevPage} className="cursor-pointer flex items-center justify-center px-4 h-10 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700  " >
+                      <Link to='#' onClick={prevPage} className="cursor-pointer flex items-center justify-center px-4 h-10 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700  " >
                         <span className="sr-only">Previous</span>
                         <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                           <path stroke="currentColor" stroke-linecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 1 1 5l4 4" />
                         </svg>
-                      </a>
+                      </Link>
                     </li>
-                    {Numbers?.map((n, i) => (<li> <a onClick={() => changeCurrentPage(n)} className="cursor-pointer  flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700  ">{n}</a> </li>))}
+                    {Numbers?.map((n, i) => (<li> <Link onClick={() => changeCurrentPage(n)} className="cursor-pointer  flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700  ">{n}</Link> </li>))}
 
                     <li>
                       <Link onClick={nextPage} className="cursor-pointer flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700  "> <span className="sr-only">Next</span><svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10"> <path stroke="currentColor" stroke-linecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 9 4-4-4-4" /> </svg></Link>
