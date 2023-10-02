19ea3efa31fb0f45d6d131dc0b5b00c863343db1
AhmadHassan <AhmadHassan25202@gmail.com>
2023-10-02 01:55:32 -0700

    providerPanel

--- src/dashboard/Categories.jsx
+++ src/dashboard/Categories.jsx
@@ -175,7 +175,7 @@ const Categories = () => {
                           <span className=' text-[13px] font-[350]'>{value.jobs}</span>
                         </td>
 
-                        <td className="py-[2%] max-md:text-[.7rem] w-[2%] border-r-[1px] border-t-[1px]   text-center">
+                        <td className="py-[2%] max-md:text-[.7rem] w-[2%] border-r-[1px] border-t-[1px] text-center">
                           <span onClick={() => UpdateStatus(value.id, value.status)} className={`bg-green-600 text-white font-[500] py-[3px] px-[10px] max-md:w-[8%] rounded-xl text-[0.6rem] max-md:py-1 max-md:px-2 max-md:text-[0.6rem] cursor-pointer ${value.status === 1 ? 'bg-green-500' : 'bg-red-500'} `}>{value.status === 1 ? 'Enable' : 'Disable'}</span>
                         </td>
                         <td className="py-[2%] w-[2%] max-md:text-[.7rem]  border-r-[1px] border-t-[1px]   text-center">
