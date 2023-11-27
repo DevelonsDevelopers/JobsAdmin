a9cceb8af41e6b590d4590a1503c6a9ee7af8caf
username880 <abubakar.4983763@gmail.com>
2023-11-27 14:53:34 +0500

    removed unwanted pages

---
+++ src/dashboard/NoPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const NoPage = () => {
+    return (
+        <div>
+            <center className='mt-[5%]'>
+                <img src="./assets/baba-g.jpg" alt="" className='rounded-xl' />
+                <p className='mt-4 font-bold text-[1.4rem]'>chala jao beta yaha sa </p>
+            </center>
+        </div>
+    )
+}
+
+export default NoPage
