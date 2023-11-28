8d2cc8633d3223867fc960e02df7ff6ef93f27ca
username880 <abubakar.4983763@gmail.com>
2023-11-28 18:26:35 +0500

    provider panel updated ui

--- src/ProviderDashboard/Interactions.jsx
+++ src/ProviderDashboard/Interactions.jsx
@@ -95,11 +95,10 @@ const ProviderInteraction = () => {
       </div>
       </center> :
         <>
-          {nodata ? <center> <div className=" pt-[10%]" > <img src="./assets/nodata3.png" alt="no image" className="opacity-75 w-[60%] h-[50%] mt-[-10%]" />
-            <h1 className=" text-[2rem] text-gray-500 mt-[-4rem] pt-10" >No User Found</h1>
-
-
-          </div> </center>
+          {nodata ? 
+         <>
+         <h1 className=" text-[2rem] text-gray-500 " >No Interactions Yet</h1>
+       </>
             :
             <>
 
