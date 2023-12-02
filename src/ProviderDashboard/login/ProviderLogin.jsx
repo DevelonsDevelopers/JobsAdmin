c2fe2c07946a28573a8428f43aed89fa2b308c33
AhmadHassan <116072911+AhmadHassanMughal@users.noreply.github.com>
2023-12-01 16:31:10 -0930

    login

--- src/ProviderDashboard/login/ProviderLogin.jsx
+++ src/ProviderDashboard/login/ProviderLogin.jsx
@@ -1,52 +1,134 @@
-import React, { useEffect, useState } from "react"
-import { useNavigate } from "react-router-dom"
-import { CompanyLogin } from "../../store"
-import { AiFillEye, AiFillEyeInvisible, AiOutlineMail } from 'react-icons/ai'
-import { RiLockPasswordLine } from 'react-icons/ri'
-import { ToastContainer, toast } from "react-toastify"
-import AppView from "../view/AppView"
-import axios from "axios"
-import { useGoogleLogin } from "@react-oauth/google"
-import { SESSION_PROVIDER, SESSION_PROVIDER_ID, SESSION_PROVIDER_LOGIN, SESSION_PROVIDER_TYPE, SESSION_ADMIN_LOGIN } from "../../Utils/Constant";
-import picture from '../../assets/panelLogin.png'
-import googleImg from '../../assets/google.png'
-import { IoMail } from "react-icons/io5"
-import { FaKey } from "react-icons/fa"
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { CompanyLogin } from "../../store";
+import { AiFillEye, AiFillEyeInvisible, AiOutlineMail } from "react-icons/ai";
+import { RiLockPasswordLine } from "react-icons/ri";
+import { ToastContainer, toast } from "react-toastify";
+import AppView from "../view/AppView";
+import axios from "axios";
+import { useGoogleLogin } from "@react-oauth/google";
+import {
+  SESSION_PROVIDER,
+  SESSION_PROVIDER_ID,
+  SESSION_PROVIDER_LOGIN,
+  SESSION_PROVIDER_TYPE,
+  SESSION_ADMIN_LOGIN,
+} from "../../Utils/Constant";
+import picture from "../../assets/login-profile.png";
+import googleImg from "../../assets/google.png";
+import { IoMail } from "react-icons/io5";
+import { FaKey } from "react-icons/fa";
 
 const ProviderLogin = () => {
-  const [loginData, setLoginData] = useState({ email: '', password: '' })
-  const navigate = useNavigate()
+  const [loading, setLoading] = useState(false);
+
+  const [loginData, setLoginData] = useState({ email: "", password: "" });
+  const navigate = useNavigate();
   const handleChange = (e) => {
-    setLoginData(prev => ({ ...prev, [e.target.name]: e.target.value }))
-  }
+    setLoginData((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
 
   useEffect(() => {
-    const isLogin = sessionStorage.getItem(SESSION_PROVIDER_LOGIN)
-    const loginType = sessionStorage.getItem(SESSION_PROVIDER_TYPE)
+    const isLogin = sessionStorage.getItem(SESSION_PROVIDER_LOGIN);
+    const loginType = sessionStorage.getItem(SESSION_PROVIDER_TYPE);
     if (isLogin === "true" && loginType === "PROVIDER") {
-      navigate('/providerPanel')
+      navigate("/providerPanel");
     } else {
     }
-  }, [])
+  }, []);
 
   const ProviderLogin = () => {
-    CompanyLogin(loginData.email, loginData.password).then(res => {
-      console.log(res)
-      const { data: { data } } = res;
-      const { data: { status } } = res;
-      console.log('res', status)
-      if (status === 'OK') {
-        sessionStorage.setItem(SESSION_PROVIDER_LOGIN, "true")
-        sessionStorage.setItem(SESSION_PROVIDER_ID, data.id)
-        sessionStorage.setItem(SESSION_PROVIDER_TYPE, "PROVIDER")
-        sessionStorage.setItem(SESSION_PROVIDER, JSON.stringify(data))
-        sessionStorage.setItem(SESSION_ADMIN_LOGIN, "false")
-        navigate('/providerPanel')
-      }
-      else {
-        toast.error('Invalid Username or Password', {
+    setLoading(true);
+    CompanyLogin(loginData.email, loginData.password).then((res) => {
+      console.log(res);
+      const {
+        data: { data },
+      } = res;
+      const {
+        data: { status },
+      } = res;
+      console.log("res", status);
+      // if (status === "OK") {
+      //   sessionStorage.setItem(SESSION_PROVIDER_LOGIN, "true");
+      //   sessionStorage.setItem(SESSION_PROVIDER_ID, data.id);
+      //   sessionStorage.setItem(SESSION_PROVIDER_TYPE, "PROVIDER");
+      //   sessionStorage.setItem(SESSION_PROVIDER, JSON.stringify(data));
+      //   sessionStorage.setItem(SESSION_ADMIN_LOGIN, "false");
+      //   navigate("/providerPanel");
+      // } else {
+      //   toast.error("Invalid Username or Password", {
+      //     position: "top-right",
+      //     autoClose: 2000,
+      //     hideProgressBar: false,
+      //     closeOnClick: true,
+      //     pauseOnHover: true,
+      //     draggable: true,
+      //     progress: undefined,
+      //     theme: "light",
+      //   });
+      // }
+      if (loginData?.email?.length || loginData?.password?.length !== 0) {
+        if (loginData?.email?.length != 0) {
+          if (loginData?.password?.length != 0) {
+            if (status === "OK") {
+              setLoading(false);
+              sessionStorage.setItem(SESSION_PROVIDER_LOGIN, "true");
+              sessionStorage.setItem(SESSION_PROVIDER_ID, data.id);
+              sessionStorage.setItem(SESSION_PROVIDER_TYPE, "PROVIDER");
+              sessionStorage.setItem(SESSION_PROVIDER, JSON.stringify(data));
+              sessionStorage.setItem(SESSION_ADMIN_LOGIN, "false");
+              navigate("/providerPanel",
+                toast.success("Login Successfully", {
+                  position: "top-right",
+                  autoClose: 1000,
+                  hideProgressBar: false,
+                  closeOnClick: true,
+                  pauseOnHover: true,
+                  draggable: true,
+                  progress: undefined,
+                  theme: "light",
+                })
+              );
+            } else {
+              toast.error("No Email or Password Exists", {
+                position: "top-right",
+                autoClose: 2000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "light",
+              });
+            }
+          } else {
+            toast.error("Enter Your Valid Password", {
+              position: "top-right",
+              autoClose: 2000,
+              hideProgressBar: false,
+              closeOnClick: true,
+              pauseOnHover: true,
+              draggable: true,
+              progress: undefined,
+              theme: "light",
+            });
+          }
+        } else {
+          toast.error("Please Enter Your Valid Email", {
+            position: "top-right",
+            autoClose: 2000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+          });
+        }
+      } else {
+        toast.error("Enter Your Valid Email Address or Password", {
           position: "top-right",
-          autoClose: 5000,
+          autoClose: 2000,
           hideProgressBar: false,
           closeOnClick: true,
           pauseOnHover: true,
@@ -55,42 +137,39 @@ const ProviderLogin = () => {
           theme: "light",
         });
       }
-    })
-  }
-
-
+      setTimeout(() => {
+        setLoading(false);
+      }, 2000);
+    });
+  };
 
   const googleLogin = useGoogleLogin({
-    onSuccess: async tokenResponse => {
+    onSuccess: async (tokenResponse) => {
       console.log(tokenResponse);
 
       // fetching userinfo can be done on the client or the server
       const userInfo = await axios
-        .get('https://www.googleapis.com/oauth2/v3/userinfo', {
+        .get("https://www.googleapis.com/oauth2/v3/userinfo", {
           headers: { Authorization: `Bearer ${tokenResponse.access_token}` },
         })
-        .then(async res => {
-
-          console.log(res)
-
+        .then(async (res) => {
+          console.log(res);
         });
       // console.log(userInfo);
     },
     // flow: 'implicit', // implicit is the default
   });
 
-  const [open, setOpen] = useState()
-  const toggleOpenVisible = () => setOpen(!open)
+  const [open, setOpen] = useState();
+  const toggleOpenVisible = () => setOpen(!open);
 
-
-  const [show, setShow] = useState(false)
+  const [show, setShow] = useState(false);
 
   const handleSubmit = () => {
-    setShow(!show)
-    myFunction()
-  }
+    setShow(!show);
+    myFunction();
+  };
   // navigate('/')
-
 
   const myFunction = () => {
     var x = document.getElementById("myInput");
@@ -99,11 +178,104 @@ const ProviderLogin = () => {
     } else {
       x.type = "password";
     }
-  }
+  };
 
   return (
     <>
-      <div>
+      <div className="bg-yellow-400 h-screen flex justify-center items-center">
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+        />
+        <div className="flex bg-white rounded-xl shadow-md shadow-gray-500 h-[30rem] w-[70%] m-auto ">
+          <div className="w-[40%] flex flex-col items-center py-10 h-full rounded-l-[10px] bg-black max-md:hidden">
+            <img src={picture} alt="" className="w-[60%] h-[60%] " />
+            <h1 className="text-white mt-10 text-[2.5rem] font-[600] uppercase">
+              Sing In
+            </h1>
+          </div>
+          <div className="w-[60%] py-10 max-md:w-[90%] max-md:m-auto flex items-center justify-center ">
+            <div className="w-[50%]">
+              <h1 className="text-[22px] font-[600] ">
+                Hello! Welcome Back :)
+              </h1>
+              <br />
+              <h1 className="text-[13px] text-gray-600 font-[600]">
+                To keep connected with us please login with your personal
+                information by email and password
+              </h1>
+              <br />
+              <div className=" px-3 shadow-lg flex gap-5 rounded-[10px] w-[100%] max-md:w-[100%]  border-2 border-gray-400 ">
+                <div className="flex items-center justify-center">
+                  <IoMail className="text-[1.7rem] text-black" />
+                </div>
+                <input
+                  type="email"
+                  name="email"
+                  onChange={handleChange}
+                  placeholder="Enter Your Email"
+                  className="py-3 text-[.9rem] w-full text-gray-700 font-[500] outline-none hover:outline-none focus:bg-white   "
+                />
+              </div>
+              <br />
+              <div className=" px-3 shadow-lg flex gap-5 rounded-[10px] w-[100%] max-md:w-[100%]  border-2 border-gray-400 ">
+                <div className="flex items-center justify-center">
+                  <FaKey className="text-[1.7rem] text-black" />
+                </div>
+                <input
+                  type="password"
+                  id="myInput"
+                  name="password"
+                  onChange={handleChange}
+                  placeholder="Enter Your Password"
+                  className="py-3 text-[.9rem] text-gray-700 font-[500] outline-none hover:outline-none "
+                />
+              </div>
+              {show ? (
+                <div
+                  className="relative top-[-2.2rem] left-[90%] w-[10%] text-[1.4rem]"
+                  onClick={() => handleSubmit()}
+                >
+                  {" "}
+                  <AiFillEye />
+                </div>
+              ) : (
+                <div
+                  className="relative top-[-2.2rem] left-[90%] w-[10%] text-[1.4rem]"
+                  onClick={() => handleSubmit()}
+                >
+                  {" "}
+                  <AiFillEyeInvisible />
+                </div>
+              )}
+              {loading ? (
+                <center>
+                  {" "}
+                  <div className="bg-black border-black text-white transition-all ease-in-out duration-150 border-2 cursor-pointer font-[600] px-10 py-[7px] w-[100%] rounded-full mt-6">
+                    <div className="animate-spin rounded-full h-4 w-4 border-t-2 border-white"></div>
+                  </div>
+                </center>
+              ) : (
+                <input
+                  type="submit"
+                  value="Sing In"
+                  onClick={() => ProviderLogin()}
+                  className="bg-white text-black hover:bg-black border-black hover:text-white transition-all ease-in-out duration-150 border-2 cursor-pointer font-[600] px-10 py-[5px] w-[100%] rounded-full mt-6"
+                />
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* <div>
         <AppView open={open} toggleOpenVisible={toggleOpenVisible} />
         <ToastContainer
           position="top-right"
@@ -154,9 +326,9 @@ const ProviderLogin = () => {
             </center>
           </div>
         </div>
-      </div>
+      </div> */}
     </>
-  )
-}
+  );
+};
 
-export default ProviderLogin
+export default ProviderLogin;
--- src/dashboard/login/Login.jsx
+++ src/dashboard/login/Login.jsx
@@ -1,52 +1,60 @@
-import React, { useEffect, useState } from "react"
-import { Link, useNavigate } from "react-router-dom"
-import { LoginUser } from "../../store"
-import { AiFillEye, AiFillEyeInvisible, AiOutlineMail } from 'react-icons/ai'
-import { RiLockPasswordLine } from 'react-icons/ri'
-import { ToastContainer, toast } from "react-toastify"
-import { SESSION_ADMIN_ID, SESSION_ADMIN_LOGIN, SESSION_ADMIN_TYPE, SESSION_ADMIN_USER, SESSION_PROVIDER_LOGIN } from "../../Utils/Constant"
-import picture from '../../assets/panelLogin.png'
-import { IoMail } from "react-icons/io5"
-import { FaKey } from "react-icons/fa"
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { LoginUser } from "../../store";
+import { AiFillEye, AiFillEyeInvisible, AiOutlineMail } from "react-icons/ai";
+import { RiLockPasswordLine } from "react-icons/ri";
+import { ToastContainer, toast } from "react-toastify";
+import {
+  SESSION_ADMIN_ID,
+  SESSION_ADMIN_LOGIN,
+  SESSION_ADMIN_TYPE,
+  SESSION_ADMIN_USER,
+  SESSION_PROVIDER_LOGIN,
+} from "../../Utils/Constant";
+import picture from "../../assets/login-profile.png";
+import { IoMail } from "react-icons/io5";
+import { FaKey } from "react-icons/fa";
 
 const Login = () => {
+  const [loading, setLoading] = useState(false);
 
-
-  const [loading, setLoading] = useState(false)
-
-
-  const [loginData, setLoginData] = useState({ email: '', password: '' })
-  const navigate = useNavigate()
+  const [loginData, setLoginData] = useState({ email: "", password: "" });
+  const navigate = useNavigate();
   const handleChange = (e) => {
-    setLoginData(prev => ({ ...prev, [e.target.name]: e.target.value }))
-  }
+    setLoginData((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
 
   useEffect(() => {
-    const isLogin = sessionStorage.getItem(SESSION_ADMIN_LOGIN)
-    const adminType = sessionStorage.getItem(SESSION_ADMIN_TYPE)
+    const isLogin = sessionStorage.getItem(SESSION_ADMIN_LOGIN);
+    const adminType = sessionStorage.getItem(SESSION_ADMIN_TYPE);
     if (isLogin === "true" && adminType === "USER") {
-      navigate('/userPanel')
+      navigate("/userPanel");
     } else {
     }
-  }, [])
+  }, []);
 
   const login = () => {
-    setLoading(true)
-    LoginUser(loginData.email, loginData.password).then(res => {
-      const { data: { data } } = res
-      const { data: { status } } = res
+    setLoading(true);
+    LoginUser(loginData.email, loginData.password).then((res) => {
+      const {
+        data: { data },
+      } = res;
+      const {
+        data: { status },
+      } = res;
       if (loginData?.email?.length || loginData?.password?.length !== 0) {
         if (loginData?.email?.length != 0) {
           if (loginData?.password?.length != 0) {
-            if (status === 'OK') {
-              setLoading(false)
-              sessionStorage.setItem(SESSION_ADMIN_LOGIN, "true")
-              sessionStorage.setItem(SESSION_ADMIN_ID, data.id)
-              sessionStorage.setItem(SESSION_ADMIN_TYPE, "USER")
-              sessionStorage.setItem(SESSION_ADMIN_USER, JSON.stringify(data))
-              sessionStorage.setItem(SESSION_PROVIDER_LOGIN, "false")
-              navigate('/userPanel',
-                toast.success('Login Successfully', {
+            if (status === "OK") {
+              setLoading(false);
+              sessionStorage.setItem(SESSION_ADMIN_LOGIN, "true");
+              sessionStorage.setItem(SESSION_ADMIN_ID, data.id);
+              sessionStorage.setItem(SESSION_ADMIN_TYPE, "USER");
+              sessionStorage.setItem(SESSION_ADMIN_USER, JSON.stringify(data));
+              sessionStorage.setItem(SESSION_PROVIDER_LOGIN, "false");
+              navigate(
+                "/userPanel",
+                toast.success("Login Successfully", {
                   position: "top-right",
                   autoClose: 1000,
                   hideProgressBar: false,
@@ -55,9 +63,10 @@ const Login = () => {
                   draggable: true,
                   progress: undefined,
                   theme: "light",
-                }))
+                })
+              );
             } else {
-              toast.error('No Email or Password Exists', {
+              toast.error("No Email or Password Exists", {
                 position: "top-right",
                 autoClose: 2000,
                 hideProgressBar: false,
@@ -68,9 +77,8 @@ const Login = () => {
                 theme: "light",
               });
             }
-
           } else {
-            toast.error('Enter Your Valid Password', {
+            toast.error("Enter Your Valid Password", {
               position: "top-right",
               autoClose: 2000,
               hideProgressBar: false,
@@ -81,9 +89,8 @@ const Login = () => {
               theme: "light",
             });
           }
-
         } else {
-          toast.error('Please Enter Your Valid Email', {
+          toast.error("Please Enter Your Valid Email", {
             position: "top-right",
             autoClose: 2000,
             hideProgressBar: false,
@@ -94,9 +101,8 @@ const Login = () => {
             theme: "light",
           });
         }
-      }
-      else {
-        toast.error('Enter Your Valid Email Address or Password', {
+      } else {
+        toast.error("Enter Your Valid Email Address or Password", {
           position: "top-right",
           autoClose: 2000,
           hideProgressBar: false,
@@ -110,17 +116,16 @@ const Login = () => {
       setTimeout(() => {
         setLoading(false);
       }, 2000);
-    })
-  }
+    });
+  };
 
-  const [show, setShow] = useState(false)
+  const [show, setShow] = useState(false);
 
   const handleSubmit = () => {
-    setShow(!show)
-    myFunction()
-  }
+    setShow(!show);
+    myFunction();
+  };
   // navigate('/')
-
 
   function myFunction() {
     var x = document.getElementById("myInput");
@@ -133,7 +138,7 @@ const Login = () => {
 
   return (
     <>
-      <div>
+      <div className="bg-yellow-400 h-screen flex justify-center items-center">
         <ToastContainer
           position="top-right"
           autoClose={5000}
@@ -146,46 +151,84 @@ const Login = () => {
           pauseOnHover
           theme="light"
         />
-        <div className='w-[100%] h-[24rem] bg-blue-600'>
-        </div>
-        <div className=' bg-gray-400 '>
-          <div className="flex bg-white py-10 rounded-xl w-[60%] relative top-[-250px] m-auto ">
-            <div className="w-[50%] mt-[10%] max-md:hidden">
-              <img src={picture} alt="" className="w-160 " />
-            </div>
-            <center className="w-[40%] max-md:w-[90%] max-md:m-auto">
-              <div className=' p-[10px]'><br />
-                <h1 className='text-[22px]'>Welcome Back :)</h1><br />
-                <h1 className='text-[13px] text-gray-600 font-[600]'>To keep connected with us please login with your personal information by email and password</h1><br />
-                <input type="email" name="email" onChange={handleChange} placeholder='Enter Your Email' className='shadow-lg rounded-[12px] text-[.9rem] text-gray-700 font-[500] w-[100%] max-md:w-[100%] pl-[50px] border-2 pt-[7px] pb-[7px]' /><br />
-                <div className="relative top-[-1.9rem] left-[-44%] w-[10%]"> <IoMail />
+        <div className="flex bg-white rounded-xl shadow-md shadow-gray-500 h-[30rem] w-[70%] m-auto ">
+          <div className="w-[40%] flex flex-col items-center py-10 h-full rounded-l-[10px] bg-black max-md:hidden">
+              <img src={picture} alt="" className="w-[60%] h-[60%] " />
+              <h1 className="text-white mt-10 text-[2.5rem] font-[600] uppercase" >Sing In</h1>
+          </div>
+          <div className="w-[60%] py-10 max-md:w-[90%] max-md:m-auto flex items-center justify-center ">
+            <div className="w-[50%]">
+              <h1 className="text-[22px] font-[600] ">Hello! Welcome Back :)</h1>
+              <br />
+              <h1 className="text-[13px] text-gray-600 font-[600]">
+                To keep connected with us please login with your personal
+                information by email and password
+              </h1>
+              <br />
+              <div className=" px-3 shadow-lg flex gap-5 rounded-[10px] w-[100%] max-md:w-[100%]  border-2 border-gray-400 ">
+                <div className="flex items-center justify-center">
+                  <IoMail className="text-[1.7rem] text-black" />
                 </div>
-                <input type="password" id="myInput" name="password" onChange={handleChange} placeholder='Password' className='shadow-lg rounded-[12px] text-[.9rem] text-gray-700 font-[500] w-[100%] pr-[50px] max-md:w-[100%] pl-[50px] border-2 pt-[7px] pb-[7px]' /><br />
-                <div className="relative top-[-1.9rem] left-[-44%] w-[10%]"> <FaKey />
+                <input
+                  type="email"
+                  name="email"
+                  onChange={handleChange}
+                  placeholder="Enter Your Email"
+                  className="py-3 text-[.9rem] w-full text-gray-700 font-[500] outline-none hover:outline-none focus:bg-white   "
+                />
+              </div>
+              <br />
+              <div className=" px-3 shadow-lg flex gap-5 rounded-[10px] w-[100%] max-md:w-[100%]  border-2 border-gray-400 ">
+                <div className="flex items-center justify-center">
+                  <FaKey className="text-[1.7rem] text-black" />
                 </div>
-                {show ?
-                  <div className="relative top-[-2.9rem] left-[44%] w-[10%] text-[1.4rem]" onClick={() => handleSubmit()}> <AiFillEye />
-                  </div>
-                  :
-                  <div className="relative top-[-2.9rem] left-[44%] w-[10%] text-[1.4rem]" onClick={() => handleSubmit()}> <AiFillEyeInvisible />
-                  </div>
-                }
-                {loading ?
-                  <center> <div className="flex justify-center items-center bg-blue-600 px-10 py-[8px] w-[100%] rounded-full mt-6">
+                <input
+                  type="password"
+                  id="myInput"
+                  name="password"
+                  onChange={handleChange}
+                  placeholder="Enter Your Password"
+                  className="py-3 text-[.9rem] text-gray-700 font-[500] outline-none hover:outline-none "
+                />
+              </div>
+              {show ? (
+                <div
+                  className="relative top-[-2.2rem] left-[90%] w-[10%] text-[1.4rem]"
+                  onClick={() => handleSubmit()}
+                >
+                  {" "}
+                  <AiFillEye />
+                </div>
+              ) : (
+                <div
+                  className="relative top-[-2.2rem] left-[90%] w-[10%] text-[1.4rem]"
+                  onClick={() => handleSubmit()}
+                >
+                  {" "}
+                  <AiFillEyeInvisible />
+                </div>
+              )}
+              {loading ? (
+                <center>
+                  {" "}
+                  <div className="bg-black border-black text-white transition-all ease-in-out duration-150 border-2 cursor-pointer font-[600] px-10 py-[7px] w-[100%] rounded-full mt-6">
                     <div className="animate-spin rounded-full h-4 w-4 border-t-2 border-white"></div>
                   </div>
-                  </center>
-                  :
-                  <input type='submit' value='Log In' onClick={() => login()} className='bg-blue-600 text-white cursor-pointer font-[600] px-10 py-[5px] w-[100%] rounded-full mt-6' />
-                }
-              </div>
-
-            </center>
+                </center>
+              ) : (
+                <input
+                  type="submit"
+                  value="Sing In"
+                  onClick={() => login()}
+                  className="bg-white text-black hover:bg-black border-black hover:text-white transition-all ease-in-out duration-150 border-2 cursor-pointer font-[600] px-10 py-[5px] w-[100%] rounded-full mt-6"
+                />
+              )}
+            </div>
           </div>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
